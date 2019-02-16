73ca7a664e48bc244ae252634800acc9aab34465
masato-ka <jp6uzv@gmail.com>
2019-02-16 14:35:56 +0900

    initial commit.

---
+++ src/main/java/ka/masato/wordcloud/wordcloudcreator/WordcloudCreatorApplication.java
@@ -0,0 +1,14 @@
+package ka.masato.wordcloud.wordcloudcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcloudCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordcloudCreatorApplication.class, args);
+    }
+
+}
+
